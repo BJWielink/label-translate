862d0be47725450b3a6473919db5047850b7bcda
Bert Jan Wielink <bertjan@digi-process.nl>
2024-04-26 08:55:56 +0200

    refactor variables

--- src/main/kotlin/org/wielink/labelTranslate/engine/TranslationFileSaver.kt
+++ src/main/kotlin/org/wielink/labelTranslate/engine/TranslationFileSaver.kt
@@ -10,8 +10,6 @@ import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.jetbrains.php.lang.psi.PhpPsiElementFactory
 import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression
 import com.jetbrains.php.lang.psi.elements.ArrayHashElement
-import com.jetbrains.php.lang.psi.elements.PhpPsiElement
-import com.jetbrains.php.lang.psi.elements.StringLiteralExpression
 import org.wielink.labelTranslate.model.node.AbstractNode
 import org.wielink.labelTranslate.model.node.LanguageNode
 import org.wielink.labelTranslate.model.node.TranslationNode
@@ -33,85 +31,65 @@ class TranslationFileSaver(
         val virtualFile = LocalFileSystem.getInstance().findFileByIoFile(languageFile) ?: return
         val psiFile = virtualFile.findPsiFile(project) ?: return
 
-        // Create a copy
-        val copy = psiFile.copy() as PsiFile
-        copy.name = "updated.php"
+        // TODO: When deserialization works, just replace the existing psi file
+        val psiOutputFile = psiFile.copy() as PsiFile
+        psiOutputFile.name = "updated.php"
 
         // Add the translations
-        traverseNodes(copy, languageNode)
+        deserializeNodeIntoPsi(psiOutputFile, languageNode)
 
-        // Format the code
+        // Add indenting to the newly created file
         val codeStyleManager = CodeStyleManager.getInstance(project)
-        codeStyleManager.reformat(copy)
+        codeStyleManager.reformat(psiOutputFile)
 
-        psiFile.containingDirectory.add(copy)
+        // Write the psi file to the disk
+        psiFile.containingDirectory.add(psiOutputFile)
 
         translationNode.translation = updatedValue
     }
 
-    private fun addCategory(element: PsiElement, node: AbstractNode) {
-        val type = PhpPsiElementFactory.createPhpPsiFromText(project, ArrayCreationExpression::class.java, "[")
+    private fun deserializeCategory(arrayElementToFill: PsiElement, nodeToDeserialize: AbstractNode) {
+        // Initialize the array
+        val array = PhpPsiElementFactory.createPhpPsiFromText(project, ArrayCreationExpression::class.java, "[")
 
-        for (child in node.children()) {
-            val label = child.label.replace("'", "\\'")
+        for (childNodeToDeserialize in nodeToDeserialize.children()) {
+            val label = childNodeToDeserialize.label.replace("'", "\\'")
             // Prepare key value pair by, firstly, adding the key
-            val keyValuePair = PhpPsiElementFactory.createPhpPsiFromText(project, ArrayHashElement::class.java, "['$label'     =>]")
-            if (child is TranslationNode) {
+            val keyValuePair = PhpPsiElementFactory.createPhpPsiFromText(project, ArrayHashElement::class.java, "['$label' =>]")
+            if (childNodeToDeserialize is TranslationNode) {
                 // Add a simple translation to the current array
-                val translation = child.translation.replace("'", "\\'")
+                val translation = childNodeToDeserialize.translation.replace("'", "\\'")
                 keyValuePair.add(PhpPsiElementFactory.createStringLiteralExpression(project, translation, true))
             } else {
                 // Create dummy that has to be filled with data
                 val categoryElement = PhpPsiElementFactory.createPhpPsiFromText(project, ArrayCreationExpression::class.java, "[]")
                 val addedElement = keyValuePair.add(categoryElement)
                 // Recursively add data to the dummy
-                addCategory(addedElement, child)
+                deserializeCategory(addedElement, childNodeToDeserialize)
             }
-            type.add(PhpPsiElementFactory.createNewLine(project))
-            type.add(keyValuePair)
-            type.add(PhpPsiElementFactory.createComma(project))
-            type.add(PhpPsiElementFactory.createNewLine(project))
+            array.add(PhpPsiElementFactory.createNewLine(project))
+            array.add(keyValuePair)
+            array.add(PhpPsiElementFactory.createComma(project))
+            array.add(PhpPsiElementFactory.createNewLine(project))
         }
 
-        val end = PhpPsiElementFactory.createFromText(project, LeafPsiElement::class.java, "]")!!
-        type.add(end)
+        // End the array
+        val arrayClosureElement = PhpPsiElementFactory.createFromText(project, LeafPsiElement::class.java, "]")!!
+        array.add(arrayClosureElement)
 
-        element.replace(type)
+        // Finally we replace ourselves (we are an empty array) with translation and other category data
+        arrayElementToFill.replace(array)
     }
 
-    private fun traverseNodes(element: PsiElement, languageNode: LanguageNode) {
+    private fun deserializeNodeIntoPsi(element: PsiElement, languageNode: LanguageNode) {
         // Find the array initializer
         if (TranslationFileParser.isInitialCategoryNode(element)) {
-            addCategory(element, languageNode)
+            deserializeCategory(element, languageNode)
             return
         }
 
         for (child in element.children) {
-            traverseNodes(child, languageNode)
+            deserializeNodeIntoPsi(child, languageNode)
         }
-    }
-
-    private fun traverseTranslationArray(element: PsiElement) {
-        if (element is ArrayHashElement) {
-            mutateKeyValue(element)
-        }
-
-        for (child in element.children) {
-            traverseTranslationArray(child)
-        }
-    }
-
-    private fun mutateKeyValue(element: PsiElement) {
-        val stringOperands = element.children
-            .filterIsInstance<PhpPsiElement>()
-            .map { it.firstPsiChild }
-            .filterIsInstance<StringLiteralExpression>()
-
-        if (stringOperands.size != 2) {
-            return
-        }
-
-        stringOperands[1].updateText("test")
-        println(element)
     }
 }
